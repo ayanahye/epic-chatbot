295289e6a86ac39d97d3f8838f9dcd3dda14439b
ayanahye <ayanahye2@gmail.com>
2022-12-29 19:42:59 -0800

    changed the model

--- app.py
+++ app.py
@@ -234,8 +234,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 # Train a Naive Bayes classifier
-from sklearn.naive_bayes import MultinomialNB
-model = MultinomialNB()
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
 # Test the model
